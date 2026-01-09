557bfc216c7c7b11144b1854fa6047494c54d08c
Ntesaa24 <notesa.aldinasari24@gmail.com>
2026-01-09 18:55:54 +0700

    finnal PetaNavigasi.kt

--- app/src/main/java/com/example/pertemuan14/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan14/view/controllNavigasi/PetaNavigasi.kt
@@ -23,3 +23,36 @@ fun DataSiswaApp(navController: NavHostController = rememberNavController(), mod
 	HostNavigasi(navController = navController)
 }
 
+@Composable
+fun HostNavigasi(
+	navController: NavHostController,
+	modifier: Modifier = Modifier
+){
+	NavHost(navController = navController, startDestination = DestinasiHome.route,
+		modifier = Modifier){
+		composable(DestinasiHome.route){
+			HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+				navigateToItemUpdate = {
+					navController.navigate("${DestinasiDetail.route}/${it}") })
+		}
+		composable(DestinasiEntry.route) {
+			EntrySiswaScreen(navigateBack = {
+				navController.navigate(DestinasiHome.route)
+			})
+		}
+		composable(DestinasiDetail.routeWithArgs, arguments = listOf(navArgument
+			(DestinasiDetail.itemIdArg) {
+			type = NavType. StringType})
+		){
+			DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit
+				.route}/$it")},
+				navigateBack = { navController.navigate(DestinasiHome.route) })
+		}
+		composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument
+			(DestinasiEdit.itemIdArg){
+			type= NavType.StringType})){
+			EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) },
+				onNavigateUp = { navController.navigateUp() })
+		}
+	}
+}
