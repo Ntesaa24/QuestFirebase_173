ff3e28ad1aebf80c452842c0c57dd1cb0f9cfbd1
Ntesaa24 <notesa.aldinasari24@gmail.com>
2026-01-09 18:55:20 +0700

    mengupdate PetaNavigasi.kt

--- app/src/main/java/com/example/pertemuan14/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan14/view/controllNavigasi/PetaNavigasi.kt
@@ -1,52 +1,25 @@
 package com.example.pertemuan14.view.controllNavigasi
 
+import android.R.attr.type
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.pertemuan14.view.DetailSiswaScreen
+import com.example.pertemuan14.view.EditSiswaScreen
 import com.example.pertemuan14.view.EntrySiswaScreen
 import com.example.pertemuan14.view.HomeScreen
 import com.example.pertemuan14.view.route.DestinasiDetail
+import com.example.pertemuan14.view.route.DestinasiEdit
 import com.example.pertemuan14.view.route.DestinasiEntry
 import com.example.pertemuan14.view.route.DestinasiHome
 
 @Composable
-fun DataSiswaApp(
-	navController: NavHostController = rememberNavController(),
-	modifier: Modifier
-) {
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
 	HostNavigasi(navController = navController)
 }
 
-@Composable
-fun HostNavigasi(
-	navController: NavHostController,
-	modifier: Modifier = Modifier
-) {
-	NavHost(
-		navController = navController,
-		startDestination = DestinasiHome.route,
-		modifier = Modifier
-	) {
-		composable(DestinasiHome.route) {
-			HomeScreen(
-				navigateToItemEntry = {
-					navController.navigate(DestinasiEntry.route)
-				},
-				navigateToItemUpdate = { it ->
-					navController.navigate("${DestinasiDetail.route}/$it")
-				}
-			)
-		}
-
-		composable(DestinasiEntry.route) {
-			EntrySiswaScreen(
-				navigateBack = {
-					navController.navigate(DestinasiHome.route)
-				}
-			)
-		}
-	}
-}
